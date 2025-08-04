42b6098d06fed9ef6f514fcd8a977880748c2c06
Krish-Jha-15 <24dec050@lnmiit.ac.in>
2025-08-05 00:46:45 +0530

    Remove unnecessary logging middleware from getMyPayments route for cleaner code

--- src/router/payment.router.js
+++ src/router/payment.router.js
@@ -11,19 +11,14 @@ import {
 const router = Router();
 
 // This is the intermediate middleware we added to check req.user
-const logUser = (req, res, next) => {
-    console.log("==================================================");
-    console.log("🔍 [Route Log] After 'verifyjwt', req.user is:", req.user);
-    console.log("==================================================");
-    next();
-};
+
 
 router.post("/createpayment", verifyjwt, NewPaymentRequest);
 router.patch("/update/:paymentId", verifyjwt, updaterequest);
 router.get("/delete/:paymentId", verifyjwt, deleterequest);
 
 // Note the `logUser` middleware inserted here
-router.get("/getmypayment", verifyjwt, logUser, getMyPayments);
+router.get("/getmypayment", verifyjwt, getMyPayments);
 
 router.get("/getpaymentbyid/:id", verifyjwt, getpaymentbyid);
 
